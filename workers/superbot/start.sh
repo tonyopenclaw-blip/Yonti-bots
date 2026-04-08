4b5bafa19eb972affd28c04d4ef3cd2524c5f5fe
Jenkins <jenkins@yonti.trade>
2026-04-08 15:00:02 +0000

    Auto-backup 2026-04-08 15:00

--- workers/superbot/start.sh
+++ workers/superbot/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export KALSHI_ACCESS_KEY=e275fa0a-90e0-4eaa-9fb1-d25c9f8ed804
+export KALSHI_ACCESS_KEY=f085af89-0df7-44e2-9bb3-4af0435cbfda
 cd /home/ubuntu/.openclaw/workspace/workers/superbot
 python3 superbot.py >> logs/superbot.log 2>&1
